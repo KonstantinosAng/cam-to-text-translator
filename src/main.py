ed4d14cb6cf53ca839a9b6a3dd428184ff9e12c2
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-20 23:47:54 +0200

    :zap: added arg options

--- src/main.py
+++ src/main.py
@@ -16,8 +16,8 @@ def translate(word, language):
   return translator.translate(word, lang_src='en', lang_tgt=language)
 
 
-def video(language):
-  cap = cv2.VideoCapture('http://192.168.1.17:4747/mjpegfeed?640x480')
+def video(device, language):
+  cap = cv2.VideoCapture(device)
   skip, count = 100, 0
   while cap.isOpened():
     ret, image = cap.read()
@@ -35,7 +35,7 @@ def video(language):
   cap.release()
 
 
-def find_text(image, language):
+def find_text(image, language, show=False):
   full_text = pytesseract.image_to_string(image)
   rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
   results = pytesseract.image_to_data(rgb, output_type=Output.DICT)
@@ -52,13 +52,28 @@ def find_text(image, language):
       text = translate(text, language)
       text = text if type(text) == str else text[0]
       cv2.rectangle(image, (x, y), (x + w, y + h), (255, 255, 255), -1)
-      cv2.putText(image, text, (x, y+10), cv2.FONT_ITALIC, .4, (255, 0, 0), 1)  
+      cv2.putText(image, text, (x, y+h//2), cv2.FONT_ITALIC, .4, (255, 0, 0), 1)  
+
+  if show:
+    cv2.imshow('Translated Image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
   return image
 
 if __name__ == "__main__":
   from optparse import OptionParser
   parser = OptionParser()
-  parser.add_option('--language', action='store', default='en', type='string', dest='language', help='es, ru')
+  parser.add_option('-l', action='store', default='en', type='string', dest='language', help='es, ru')
+  parser.add_option('-i', action='store', default='no', type='string', dest='image', help='yes or no')
+  parser.add_option('-p', action='store', default='d:/dev/real-time-cam-text-translator/src/img/img1.jpg', type='string', dest='image_path', help='image absolute path')
+  parser.add_option('-v', action='store', default='no', type='string', dest='video', help='yes or no')
+  parser.add_option('-d', action='store', default='http://192.168.1.17:4747/mjpegfeed?640x480', type='string', dest='device', help='video device, 0 for laptop camera or ip address for ip camera')
+
   (options, args) = parser.parse_args()
-  video(options.language)
+
+  if options.image == 'yes':
+    find_text(cv2.imread(options.image_path), options.language, show=True)
+  
+  if options.video == 'yes':
+    video(options.device, options.language)
